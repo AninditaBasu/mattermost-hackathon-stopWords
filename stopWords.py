11781ee3f89bf66ebd9c2b11162a588a98b237b6
Anindita Basu <anindita.basu@live.com>
2020-10-21 16:16:09 +0530

    Update stopWords.py

--- stopWords.py
+++ stopWords.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def stop_words():
     URL_prefix = 'https://api.github.com/search/code?q='
-    URL_suffix = '+repo:mkdocs-monorepo-plugin/docs'
+    URL_suffix = '+repo:spotify/mkdocs-monorepo-plugin/docs'
 
     reportfile = open('./templates/stopWordsSearch.html', 'w')
     reportfile.write('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1">')
